1418febf65631b887ff70da4ddc2f9bede6ff3d0
ken zou <kzou100@qc.cuny.edu>
2017-08-17 22:00:06 -0400

    Update chapter13.js

--- chapter13.js
+++ chapter13.js
@@ -5,11 +5,29 @@
  *    - You may rename the parameters.
  *    - DO NOT modify the number of parameters for each function.
  */
-
+var currentTable=document.querySelector("table");
+ var newtable;
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+  	newtable=document.createElement("table");
+    for(var i in MOUNTAINS){
+   	var tr=document.createElement("tr");
+   	var th1=document.createElement("th");
+   	var th2=document.createElement("th");
+   	var th3=document.createElement("th");
+   	var newcontent=document.createTextNode(MOUNTAINS[i].name);
+   		var newcontent1=document.createTextNode(MOUNTAINS[i].height);
+   			var newcontent2=document.createTextNode(MOUNTAINS[i].country);
+   		th1.appendChild(newcontent);
+   		th2.appendChild(newcontent1);
+   		th3.appendChild(newcontent2);
+    	tr.appendChild(th1);
+    		tr.appendChild(th2);
+    			tr.appendChild(th3);
+    newtable.appendChild(tr);
+    }
+return newtable;
 }
 
 
