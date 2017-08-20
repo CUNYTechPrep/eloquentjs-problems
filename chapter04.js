540d8fb92708ed9eeea2d5e218cb1c7ed39148b1
Maria Romanova <aemulare@users.noreply.github.com>
2017-08-19 20:26:51 -0400

    Eloquent Javascript problems

--- chapter04.js
+++ chapter04.js
@@ -9,43 +9,89 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  const max = Math.floor((end - start) / step) + 1;
+  return Array(max).fill().map((_, index) => start + (index * step));
 }
 
+
 function sum(array) {
-  // Your code here
+  let sum = 0;
+  array.forEach(element => sum += element);
+  return sum;
+  //return array.reduce((a, b) => { return a + b; }, 0);
 }
+
 
 // Problem 2: Reversing an Array
 function reverseArray(array) {
-  // Your code here
+  let newArray = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
 }
 
+
 function reverseArrayInPlace(array) {
-  // Your code here
+  let mid = Math.floor(array.length / 2);
+  let temp;
+  for (var i = 0; i < mid; i++) {
+    temp = array[array.length - 1 - i];
+    array[array.length - 1 - i] = array[i];
+    array[i] = temp;
+  }
 }
+
 
 // Problem 3: A List
 function arrayToList(array) {
-  // Your code here
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest:list};
+  }
+  return list;
 }
+
 
 function listToArray(list) {
-  // Your code here
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
 }
+
 
 function nth(list, position) {
-  // Your code here
+  if (!list)
+      return undefined;
+  else if (position === 0)
+      return list.value;
+  else
+      return nth(list.rest, position - 1);
 }
+
 
 function prepend(element, list) {
-  // Your code here
+  return { value: element, rest: list };
 }
 
+
 // Problem 4: Deep comparison
-function deepEqual(obj1, obj2) {
-  // Your code here
-}
+function deepEqual(a, b) {
+  if (a === b)
+    return true;
+
+  let propCountA = Object.keys(a).length;
+  let propCountB = Object.keys(b).length;
+
+  // if one of the parameters is null, not an object or has mismatched number of properties
+  if (a === null || b === null || typeof a !== "object" || typeof b !== "object" || propCountA !== propCountB)
+    return false;
+
+  for (let property in a)
+      return deepEqual(a[property], b[property]);
+ } 
 
 
 // Do not modify below here.
--- chapter13.js
+++ chapter13.js
@@ -9,7 +9,36 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+  let table = document.createElement("table");                // table
+    table.style.borderCollapse = "collapse";
+        let tr = document.createElement("tr");                // row element
+        let headers = Object.keys(data[0]);                   // headers content
+
+        for (let header of headers) {
+            let th = document.createElement("th");            // header element
+            th.style.border = "1px solid black";
+            th.style.padding = "3px 8px";
+            th.style.textAlign = "left";
+            th.appendChild(document.createTextNode(header));
+            tr.appendChild(th);
+        }
+        table.appendChild(tr);                                // add header row to the table
+        
+        data.forEach(dataRow => {                             // form data rows with content and style
+            tr = document.createElement("tr");
+            for (let header of headers) {
+                let td = document.createElement("td");        // cell element
+                td.style.border = "1px solid black";
+                td.style.padding = "3px 8px";
+                td.appendChild(document.createTextNode(dataRow[header]));
+                if (typeof dataRow[header] === "number") {
+                    td.style.textAlign = "right";
+                }
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        });
+        return table;
 }
 
 
