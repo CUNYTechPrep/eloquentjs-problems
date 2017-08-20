d089f4a4c8e27cb99ef380ada705255cf476865b
jamesw8 <jameswong8@gmail.com>
2017-08-19 20:08:00 -0400

    Updated problems

--- chapter05.js
+++ chapter05.js
@@ -21,12 +21,25 @@ ancestry.forEach(function(person) {
 // Problem 1: Flattening
 function flatten(arrays) {
   // Your code here
+ 	var array = [];
+	array = arrays.reduce(function red(a,b) {
+ 		return a.concat(b);
+	})
+	return array;
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
   // Your code here
+  ancest = ancestry.filter(function(person) {
+  return (byName[person.mother]!=null) &&
+  (byName[person.mother].born!=null) && 
+  (person.born!=null);
+});
+	return average(ancest.map(function(person) {
+  	return person.born-byName[person.mother].born;
+	}));
 }
 
 // Problem 3: Historical life expectancy
@@ -35,6 +48,19 @@ function averageMomChildAgeDiff() {
  */
 function averageAgeByCentury() {
   // Your code here
+  var cen = {};
+	ancestry.forEach(function(person) {
+    if (cen[Math.ceil(person.died/100)] != null) {
+      cen[Math.ceil(person.died/100)].push(person.died-person.born);
+    } else {
+      cen[Math.ceil(person.died/100)] = [];
+      cen[Math.ceil(person.died/100)].push(person.died-person.born);
+    }
+	});
+	for (var century in cen) {
+		cen[century] = average(cen[century]);
+	}
+	return cen;
 }
 
 
--- chapter13.js
+++ chapter13.js
@@ -10,8 +10,34 @@
 // Problem 1: Build table
 function buildTable(data) {
   // Your code here
-}
+  var table = document.createElement('table');
+    var row = document.createElement('tr');
+    var header;
+    for (var prop in data[0]) {
+      header = document.createElement('th');
+      header.appendChild(document.createTextNode(prop));
+      row.appendChild(header);
+	}
+    table.appendChild(row);
 
+    for (var i = 0; i < data.length; i++) {
+      row = document.createElement('tr');
+      var cell;
+      for (var prop in data[i]) {
+      	cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(data[i][prop]));
+        row.appendChild(cell);
+      }
+      table.appendChild(row);
+    }
+    var td = table.getElementsByTagName("td");
+	for (var i = 0; i < td.length; i++) {
+		if (!isNaN(td[i].textContent)){
+			td[i].style.textAlign = "right";
+        }
+	}
+    return table;
+}
 
 // Do not modify below here.
 module.exports = { buildTable };
