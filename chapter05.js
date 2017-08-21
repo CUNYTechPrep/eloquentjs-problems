ce6f8afae34a71d8a46a7df13a5ff9fe4df162d4
KirstynNatavio <kirstynatavio@hotmail.com>
2017-08-21 14:25:12 -0400

    Running tests

--- chapter05.js
+++ chapter05.js
@@ -21,12 +21,56 @@ ancestry.forEach(function(person) {
 // Problem 1: Flattening
 function flatten(arrays) {
   // Your code here
+  console.log(arrays.reduce(function(a, b) {
+	return a.concat(b);
+  }));
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
-  // Your code here
+	// Your code here
+	var ancestry = JSON.parse(ANCESTRY_FILE); 
+
+	function average(array) {
+	  function plus(a, b) { return a + b; }
+	  return array.reduce(plus) / array.length;
+	}
+
+	var byName = {};
+	ancestry.forEach(function(person) {
+	  byName[person.name] = person;
+	});
+
+
+	// Getting all the mothers' names – not all of them are valid
+	// We cannot tell what is the age of the mother when she gave birth unless we have data of them being a child and a mother
+	var all_mothers = ancestry.map(function(person) {
+		return person.mother;
+	});
+
+	// Getting records of dates when mothers were born – all of them are valid 
+	var records_born = {};
+	for (var person in byName) {
+		if (all_mothers.includes(byName[person].name)) {
+			records_born[byName[person].name] = byName[person];
+		}
+	}
+	// Getting records of dates when mothers gave birth
+	var records_birth = {};
+	for (var person in byName) {
+		if (byName[person].mother in records_born) {
+			records_birth[byName[person].name] = byName[person];
+		}
+	}
+
+	// For each child, get difference in age and store in array
+	var diff = [];
+	for (var child in records_birth) {
+		diff.push(records_birth[child].born - records_born[records_birth[child].mother].born);
+	}
+
+	return Math.round(average(diff) * 10) / 10;
 }
 
 // Problem 3: Historical life expectancy
@@ -34,7 +78,47 @@ function averageMomChildAgeDiff() {
     for the century as the value
  */
 function averageAgeByCentury() {
-  // Your code here
+  	// Your code here
+  	var ancestry = JSON.parse(ANCESTRY_FILE);
+	var byCentury = {};
+	// Creating smaller groups(derived arrays)
+	ancestry.forEach(function(person) {
+		var century = Math.ceil(person.died / 100);
+		if ((century in byCentury) === false) {
+			byCentury[century] = [];
+		}
+		byCentury[century].push(person.died - person.born);
+	});
+
+	function average(array) {
+	  function plus(a, b) { return a + b; }
+	  return array.reduce(plus) / array.length;
+	}
+
+	for (var century in byCentury) {
+		console.log(century + ": " + Math.round(average(byCentury[century]) * 10) / 10);
+	}
+
+	// -------------------
+	function groupBy(array, action) {
+		var groups = {};
+		var length = array.length;
+		// Creating smaller groups(derived arrays)
+		for (var i = 0; i < length; ++i) {
+			var category = action(array[i]);
+			if ((category in groups) === false) groups[category] = [];
+	    groups[category].push(array[i]);
+		}
+		return groups;
+	}
+
+	var death = ancestry.map(function(person) {
+		return person.died;
+	});
+
+	return (groupBy(death, function(item){  // grouping by century of death
+		return Math.ceil(item / 100);
+	}));
 }
 
 
