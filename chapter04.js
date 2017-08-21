a88433071f6ef1de9531b67e5201d1ad1956e31b
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2017-08-21 05:25:35 -0400

    Ch.5 Prob.3 works online, comment in code askes to return an obj with keys(cent) and value(avg) but, test throws error asking for number not obj.

--- chapter04.js
+++ chapter04.js
@@ -8,43 +8,108 @@
 
 
 // Problem 1: The sum of a range
-function range(start, end, step=1) {
-  // Your code here
+function range(s, e, by){
+  var arr = [];
+  for(i = s; !((s<e && i>e) || (s>e && i<e));){
+    arr.push(i);
+    if(by == null){
+      if(s<e)
+        i++;
+      else
+        i--;
+    }
+    else
+      i+=by;
+  }
+  return arr;
 }
 
-function sum(array) {
-  // Your code here
+function sum(arr){
+  var sum = 0;
+  for(i = 0; i < arr.length; i++)
+    sum+= arr[i];
+  return sum;
 }
 
 // Problem 2: Reversing an Array
-function reverseArray(array) {
-  // Your code here
+function reverseArray(arr){
+  var n_arr = [];
+  for(i = arr.length; i > 0; i--)
+    n_arr[arr.length-i] = arr[i-1];
+  return n_arr;
 }
 
-function reverseArrayInPlace(array) {
-  // Your code here
+function reverseArrayInPlace(arr){
+  for(i = 0; i < arr.length/2; i++){
+    var temp = arr[i];
+    arr[i] = arr[arr.length-1-i];
+    arr[arr.length-1-i] = temp;
+  }
 }
+
 
 // Problem 3: A List
-function arrayToList(array) {
-  // Your code here
+function arrayToList(arr){
+  var head ={ value:arr[0], rest:null};
+  temp = head;
+  for (i = 1; i < arr.length; i++){
+    obj = {value: arr[i], rest:null};
+    temp.rest = obj;
+    temp = obj;
+  }
+  return head;
 }
 
-function listToArray(list) {
-  // Your code here
+function listToArray(list){
+  var arr = [];
+  var i = 0;
+  temp = list;
+ while(temp != null){
+    arr[i] = temp.value;
+    temp = temp.rest;
+    i++;
+  }
+  return arr;
 }
 
-function nth(list, position) {
-  // Your code here
+function prepend(element, list){
+  var head ={ value:element, rest:list};
+  return head;
 }
 
-function prepend(element, list) {
-  // Your code here
+function nth(list, n){
+  temp = list;
+  for (i = 0 ; i <= n; i++){
+    if(temp == null)
+      return "undefined";
+    else if (i == n)
+      return temp.value;
+    temp = temp.rest;
+  }
 }
+
 
 // Problem 4: Deep comparison
-function deepEqual(obj1, obj2) {
-  // Your code here
+function deepEqual(obj1, obj2){
+  if(obj1 == null || obj2 == null)
+    return true;
+  else if ( typeof obj1 != typeof obj2){
+    if(typeof obj1 != "undefined" && typeof obj2 != "undefined" )
+      return false;
+  }
+  else{
+    if (obj1.length != obj2.length)
+      return false;
+    else if (obj1.length == 1 && obj1 == obj2)
+      return true;
+    else{
+    for(x in obj1){
+      if (!deepEqual(obj1[x],obj2[x]))
+        return false;
+    }
+    return true;
+    }
+  }
 }
 
 
--- chapter05.js
+++ chapter05.js
@@ -20,13 +20,20 @@ ancestry.forEach(function(person) {
 
 // Problem 1: Flattening
 function flatten(arrays) {
-  // Your code here
+  return(arrays.reduce((first, second)=> first.concat(second)));
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
-  // Your code here
+  var has_mother = function(p) {
+    return p.mother in byName;
+};
+function age_dif(p) {
+    return p.born - byName[p.mother].born
+}
+var ave = ancestry.filter(has_mother).map(age_dif);
+return average(ave);
 }
 
 // Problem 3: Historical life expectancy
@@ -34,10 +41,27 @@ function averageMomChildAgeDiff() {
     for the century as the value
  */
 function averageAgeByCentury() {
-  // Your code here
+var cent = {};
+ancestry.forEach(p  => {
+    var c = String(Math.ceil(p.died/100));
+    if (!(c in cent)) {
+      cent [c] = [];
+    }
+    cent[c].push(p.died - p.born);
 }
+                );
 
+
+var obj = {}; 
+    for (c in cent) {
+    obj.c = average(cent[c]);
+    console.log(c, "->", average(cent[c]));
+}
+return obj;
+}
+//The comments above state to return an object but the test
+//throws out an error: value must be a number. code does work
+//with the online editor through http://eloquentjavascript.net
 
 // Do not modify below here.
 module.exports = { flatten, averageMomChildAgeDiff, averageAgeByCentury };
-
