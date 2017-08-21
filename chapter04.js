54acbb8c24e25d4b5fc6bf0502e9f77b38443f9f
waiyanphyohein <waiyanphyohein@gmail.com>
2017-08-20 20:34:49 -0400

    Added Function Codes to first two problems.

--- chapter04.js
+++ chapter04.js
@@ -9,20 +9,52 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  var increm = Math.abs(step);
+  var total= [];
+  if(start < end)
+  {
+  
+     for(var i = start; i <=end;i+=increm)
+       total.push(i); 
+  
+      return total;   
+  }
+  else if(end < start)
+  {
+      for(var i = start; i >=end; i-=increm)
+          total.push(i);
+      
+      return total;
+  }
+  else if(end === start)
+      return end;
 }
 
 function sum(array) {
-  // Your code here
+  var result = 0;
+  for(var i = 0;i < array.length;i++)
+      result+= array[i];
+
+  return result;
+
 }
 
 // Problem 2: Reversing an Array
 function reverseArray(array) {
-  // Your code here
+  var GivenArray = array;
+	var Result_Array = [];
+  
+  for(var i = GivenArray.length-1; i >= 0 ;i--)
+  	Result_Array.push(GivenArray[i]);
+	return Result_Array;
 }
 
 function reverseArrayInPlace(array) {
-  // Your code here
+  var Result_Array= [];
+  for(var i  = Existing_Array.length - 1; i >= 0; i--)
+     Result_Array.push(Existing_Array[i]);
+ 
+ return Result_Array;
 }
 
 // Problem 3: A List
