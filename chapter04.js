6eb676832969b405aa20414f9ba4c0969f13b882
Murdrum <jahuay93@gmail.com>
2017-08-21 22:07:24 -0500

    Still Need to Work on Chapt5
    averageAgeByCentury still needs to be reworked.

--- chapter04.js
+++ chapter04.js
@@ -10,41 +10,111 @@
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
   // Your code here
+  var arr= [];
+  //Two Cases
+  if(start < end) {
+    for(var i=start; i <= end;) {
+      arr.push(i);
+      i+=step;
+    }
+    return arr;
+  }
+
+  if(start > end) {
+    for(var i= start; i >= end;) {
+      arr.push(i);
+      i+=step;
+    }
+    return arr;
+  }
 }
 
 function sum(array) {
   // Your code here
+  var result = 0;
+  for(var i=0; i < array.length; i++) {
+    result+= array[i];
+  }
+
+  return result;
 }
 
 // Problem 2: Reversing an Array
 function reverseArray(array) {
   // Your code here
+  var result = [];
+  for(var i = array.length -1; i >= 0; i--){
+    result.push(array[i]);
+  }
+  return result;
 }
 
 function reverseArrayInPlace(array) {
   // Your code here
+  var i = 0;
+  len = array.length;
+  middle = Math.floor(len/2);
+  temp = null;
+
+  for(; i < middle; i+= 1) {
+    temp = array[i];
+    array[i] = array[len-1-i];
+    array[len-1-i] = temp;
+  }
 }
 
 // Problem 3: A List
 function arrayToList(array) {
   // Your code here
+  var list = null;
+  for(var i = 0; i < array.length; i++) {
+    list = prepend(array[array.length -1 -i], list);
+  }
+  return list;
 }
 
 function listToArray(list) {
   // Your code here
+  var array = [];
+  for(var node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+
+  return array;
 }
 
 function nth(list, position) {
   // Your code here
+  if(list != null) {
+      if(position > 0) {
+        return nth(list.rest, position -1);
+      } else {
+        return list.value;
+      }
+    } else {
+      return undefined;
+    }
 }
 
 function prepend(element, list) {
   // Your code here
+  list = {value: element, rest: list};
+  return list;
 }
 
 // Problem 4: Deep comparison
 function deepEqual(obj1, obj2) {
   // Your code here
+  if(obj1 === obj2) {
+    return true;
+  }
+
+  if(typeof obj1 != "object" || typeof obj2 != "object" || obj1 == null || obj2 == null || Object.keys(obj1).length != Object.keys(obj2).length) {
+    return false;
+  } else {
+    for(var prop in obj1)
+      return deepEqual(obj1[prop], obj2[prop]);
+  }//ELSE
 }
 
 
--- chapter05.js
+++ chapter05.js
@@ -17,27 +17,74 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
+const haveMother = function(person) {
+  return person.mother in byName;
+};
 
 // Problem 1: Flattening
 function flatten(arrays) {
   // Your code here
+  return arrays.reduce((acc, cur)=>acc.concat(cur), []);
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
+function ageDiff(person) {
+  return person.born - byName[person.mother].born;
+}
 function averageMomChildAgeDiff() {
-  // Your code here
+  return average(ancestry.filter(haveMother).map(ageDiff));
 }
 
 // Problem 3: Historical life expectancy
 /* This must return the object/map with centuries as keys and average age
     for the century as the value
  */
+/* const personInfo = [];
+
+ancestry.forEach(function(person) {
+  const info = {};
+  info["name"] = person.name;
+  info["centuryOfDeath"] = Math.ceil(person.died/100);
+  info["age"]= person.died - person.born;
+  personInfo.push(info);
+});
+
+const centuries = {};
+personInfo.forEach(function(person) {
+  if(!centuries[person.centuryOfDeath]) {
+    centuries[person.centuryOfDeath] = [];
+  }
+
+  centuries[person.centuryOfDeath].push(person.age);
+});
+*/
 function averageAgeByCentury() {
-  // Your code here
+
+  const personInfo = [];
+
+  ancestry.forEach(function(person) {
+    const info = {};
+    info["name"] = person.name;
+    info["centuryOfDeath"] = Math.ceil(person.died/100);
+    info["age"]= person.died - person.born;
+    personInfo.push(info);
+  });
+
+  const centuries = {};
+  personInfo.forEach(function(person) {
+    if(!centuries[person.centuryOfDeath]) {
+      centuries[person.centuryOfDeath] = [];
+    }
+
+    centuries[person.centuryOfDeath].push(person.age);
+  });
+
+  for(key in centuries)
+    console.log(key + ": " + average(centuries[key]));
+
 }
 
 
 // Do not modify below here.
 module.exports = { flatten, averageMomChildAgeDiff, averageAgeByCentury };
-
