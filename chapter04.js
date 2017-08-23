b704325340d061384bbd0da0be7be1a4810f8edb
Shelly Huang <shellyhuang81@gmail.com>
2017-08-23 00:01:41 -0400

    added solutions to ch4, 5, 13

--- chapter04.js
+++ chapter04.js
@@ -9,42 +9,94 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  var arr = [];
+  if(start <= end){
+    for(var i = start; i <= end; i += step){
+      arr.push(i);
+    }
+  }else{
+    for(var i = start; i >= end; i += step){
+      arr.push(i);
+    }
+  }
+  return arr;
 }
-
 function sum(array) {
-  // Your code here
+  var total = 0;
+  for(var i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total;
 }
-
 // Problem 2: Reversing an Array
 function reverseArray(array) {
-  // Your code here
+  var newArr = [];
+  for(var i = array.length-1; i >= 0; i--){
+    newArr.push(array[i]);
+  }
+  return newArr;
 }
 
 function reverseArrayInPlace(array) {
-  // Your code here
+   var temp;
+  var i = 0;
+  var j = array.length-1;
+  while(i < j){
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    i++;
+    j--;
+  }
 }
 
 // Problem 3: A List
 function arrayToList(array) {
-  // Your code here
+  var list;
+  if(array.length > 0){
+    list = {value: array[array.length-1], rest: null};
+    if(array.length > 1){
+      for(var i = array.length-2; i >= 0; i--){
+        list = {value: array[i], rest: list};
+      }
+    }
+  }
+  return list;
 }
 
 function listToArray(list) {
-  // Your code here
+  var arr = [];
+  for(var node = list; node; node = node.rest){
+    if(node.value != null){
+      arr.push(node.value);
+    }
+  }
+  return arr;
 }
 
 function nth(list, position) {
-  // Your code here
+  if(position === 0) return list.value;
+  else return nth(list.rest, position-1);
 }
 
 function prepend(element, list) {
-  // Your code here
+  newList = {value: element, rest: list};
+  return newList;
 }
 
 // Problem 4: Deep comparison
-function deepEqual(obj1, obj2) {
-  // Your code here
+function deepEqual(a, b) {
+   if( ((typeof(a) == "object") && (a!= null))  && ((typeof(b) == "object") && (b!= null)) ){
+    for(var keya in a){
+      for(var keyb in b){
+        if(keya == keyb) return deepEqual(a[keya], b[keyb]);
+        else if(keya != keyb) return false;
+      }
+    }
+  }
+  else{
+    return a == b;
+  }
 }
 
 
