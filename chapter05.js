190236f1c749c26b1b3061a87b945377c7c53096
Azizur Rahman <arahman010@citymail.cuny.edu>
2017-08-24 13:41:17 -0400

    All tests passed

--- chapter05.js
+++ chapter05.js
@@ -20,13 +20,21 @@ ancestry.forEach(function(person) {
 
 // Problem 1: Flattening
 function flatten(arrays) {
-  // Your code here
+ 	return arrays.reduce(function(a, b) {
+		return a.concat(b);
+	}, []);
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
-  // Your code here
+	ageDifference = []
+	for(let each in byName) {
+		if(byName[byName[each].mother] != null){
+			ageDifference.push(byName[each].born - byName[byName[each].mother].born);
+		}
+	}
+	return average(ageDifference);
 }
 
 // Problem 3: Historical life expectancy
@@ -34,7 +42,24 @@ function averageMomChildAgeDiff() {
     for the century as the value
  */
 function averageAgeByCentury() {
-  // Your code here
+	let lifeExpMap = {}
+	let century;
+	for(let each in byName){
+		century = Math.ceil(byName[each].died/100);
+		if(century in lifeExpMap){
+			lifeExpMap[century].push(byName[each].died - byName[each].born);
+		}
+		else {
+			lifeExpMap[century] = [byName[each].died - byName[each].born];
+
+		}
+	}
+
+	for (let eachCentury in lifeExpMap){
+		lifeExpMap[eachCentury] = average(lifeExpMap[eachCentury]);
+	}
+
+	return lifeExpMap;
 }
 
 
--- chapter13.js
+++ chapter13.js
@@ -9,7 +9,34 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+	let tbl = document.createElement('table');
+	let tr = document.createElement('tr');
+	let keys = Object.keys(data[0]);
+	for (let i = 0; i < keys.length; i++){
+		let th = document.createElement('th');
+		th.appendChild(document.createTextNode(keys[i]));
+		// console.log(eachKey);
+		// console.log(Object.keys(data[0]));
+		tr.appendChild(th);
+	}
+	tbl.appendChild(tr);
+
+	tr = document.createElement('tr');
+	for(let j = 0; j < data.length; j++){
+		tr = document.createElement('tr');
+		// console.log(eachObject);
+		for(var eachKey in data[j]){
+			let td = document.createElement('td');
+			td.appendChild(document.createTextNode(data[j][eachKey]));
+			tr.appendChild(td);
+			// console.log(eachKey);
+		}
+		tbl.appendChild(tr);
+	}
+	
+	
+
+	return tbl;
 }
 
 
