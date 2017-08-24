829bc65e0c024a339a206337ad65604b6af33ac3
KevinCybura <KevinCybura@gmail.com>
2017-08-24 00:21:07 -0400

    Done

--- chapter04.js
+++ chapter04.js
@@ -8,48 +8,140 @@
 
 
 // Problem 1: The sum of a range
-function range(start, end, step=1) {
-  // Your code here
+function range(start, end, step = 1) {
+    let array = [];
+
+    if (step === undefined)
+        step = 1;
+
+    if (start < end) {
+
+        for (let i = start; i <= end; i += step) {
+            array.push(i);
+        }
+
+    } else {
+
+        for (let i = start; i >= end; i += step) {
+            array.push(i);
+
+        }
+
+    }
+
+    return array;
 }
 
-function sum(array) {
-  // Your code here
+function sum(arr) {
+    let answer = 0;
+    for (let i = 0; i < arr.length; i++) {
+        answer += arr[i];
+    }
+    return answer;
 }
 
 // Problem 2: Reversing an Array
-function reverseArray(array) {
-  // Your code here
+function reverseArray(arr) {
+    let reversed = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+
+    return reversed;
 }
 
-function reverseArrayInPlace(array) {
-  // Your code here
+function reverseArrayInPlace(arr) {
+    let len = arr.length - 1
+    for (let i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[len - i];
+        arr[len - i] = temp;
+    }
 }
 
 // Problem 3: A List
-function arrayToList(array) {
-  // Your code here
+function arrayToList(arr) {
+    let list;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
+            rest: list
+        };
+        if (i == arr.length - 1) {
+            list.rest = null;
+        }
+
+    }
+
+    return list;
 }
 
 function listToArray(list) {
-  // Your code here
+    let arr = [];
+    for (let i = list; list; list = list.rest) {
+
+        arr.push(list.value);
+
+    }
+
+    return arr;
 }
 
 function nth(list, position) {
-  // Your code here
+    let x = 0;
+    for (let i = list; list; list = list.rest) {
+        if (x == position) {
+            return list.value;
+        } else {
+            x++;
+        }
+    }
 }
 
 function prepend(element, list) {
-  // Your code here
+    let newList = {
+        value: element,
+        rest: list
+    }
+    return newList
 }
 
 // Problem 4: Deep comparison
 function deepEqual(obj1, obj2) {
-  // Your code here
+    if (typeof obj1 === 'object' && typeof obj2 === 'object') {
+        for (var value in obj1) {
+            if (obj2.hasOwnProperty(value)) {
+                let contains = deepEqual(obj1[value], obj2[value])
+                if (contains) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    } else {
+        if (obj1 === obj2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 // Do not modify below here.
 module.exports = {
-  range, sum, reverseArray, reverseArrayInPlace,
-  arrayToList, listToArray, nth, prepend, deepEqual
+    range,
+    sum,
+    reverseArray,
+    reverseArrayInPlace,
+    arrayToList,
+    listToArray,
+    nth,
+    prepend,
+    deepEqual
 };
--- chapter05.js
+++ chapter05.js
@@ -8,25 +8,43 @@
 const ancestry = require('./ancestry');
 
 function average(array) {
-  function plus(a, b) { return a + b; }
-  return array.reduce(plus) / array.length;
+    function plus(a, b) {
+        return a + b;
+    }
+    return array.reduce(plus) / array.length;
 }
 
 const byName = {};
 ancestry.forEach(function(person) {
-  byName[person.name] = person;
+    byName[person.name] = person;
 });
 
 
 // Problem 1: Flattening
 function flatten(arrays) {
-  // Your code here
+    // Your code here
+    let b = arrays.reduce((x, y) => {
+        return x.concat(y);
+    });
+    return b;
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
-  // Your code here
+    var byName = {};
+    ancestry.forEach(function(person) {
+        byName[person.name] = person;
+    });
+
+    let mom = ancestry.map((person) => {
+        if (byName[person.mother] != null) {
+            return (person.born - byName[person.mother].born);
+        }
+    })
+    mom = mom.filter(age => age != undefined);
+    let av = average(mom);
+    return av;
 }
 
 // Problem 3: Historical life expectancy
@@ -34,10 +52,34 @@ function averageMomChildAgeDiff() {
     for the century as the value
  */
 function averageAgeByCentury() {
-  // Your code here
+    // Your code here
+    let byCentury = {}
+    ancestry.forEach((person) => {
+        let cent = Math.ceil(person.died / 100)
+        let age = person.died - person.born;
+        if (byCentury.hasOwnProperty(cent)) {
+            byCentury[cent].push(age);
+        } else {
+            byCentury[cent] = [];
+            byCentury[cent].push(age);
+        }
+    })
+
+    for (let century in byCentury) {
+
+
+        let av = average(byCentury[century]);
+
+        byCentury[century] = av;
+
+    }
+    return byCentury
 }
 
 
 // Do not modify below here.
-module.exports = { flatten, averageMomChildAgeDiff, averageAgeByCentury };
-
+module.exports = {
+    flatten,
+    averageMomChildAgeDiff,
+    averageAgeByCentury
+};
