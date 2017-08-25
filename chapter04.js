e46fa31805d735484778f478a93ba7b88aa74a2c
Ahmed Abdelrahman <ahmed_3m2010@hotmail.com>
2017-08-24 22:45:58 -0400

    Add solutions to chapter 4, 5, and 13 EloquentJS

--- chapter04.js
+++ chapter04.js
@@ -9,42 +9,109 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  var result = [];
+  if(start <= end) {
+    for(var i = start; i <= end; i += step) {
+    	result.push(i);
+    }
+  } else {
+    for(var i = start; i >= end; i += step) {
+    	result.push(i);
+    }
+  }
+  return result;
 }
 
 function sum(array) {
-  // Your code here
+  var total = 0;
+  for(var i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 }
 
 // Problem 2: Reversing an Array
-function reverseArray(array) {
-  // Your code here
+function reverseArray(inputArray) {
+  var reversedArray = [];
+  // looping backwards and pushing the elements to a new array
+  for(var i = inputArray.length-1; i >= 0; i--) {
+  	reversedArray.push(inputArray[i]);
+  }
+  return reversedArray;
 }
 
-function reverseArrayInPlace(array) {
-  // Your code here
+function reverseArrayInPlace(inputArray) {
+  var midIndex = Math.floor(inputArray.length / 2) - 1;
+  var lastIndex = inputArray.length - 1;
+  // swapping elements at the ends until reaching midpoint
+  for(var i = 0; i <= midIndex; i++) {
+    var temp = inputArray[i];
+    inputArray[i] = inputArray[lastIndex - i];
+    inputArray[lastIndex - i] = temp;
+  }
+  return inputArray;
 }
 
 // Problem 3: A List
 function arrayToList(array) {
-  // Your code here
+  var list = null;
+  // loop the array backward and keep adding elements to the head of the list
+  for(var i = array.length-1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
+  }
+  return list;
 }
 
 function listToArray(list) {
-  // Your code here
+  var array = [];
+  var node = list;	// start at the head of the list
+  // loop until the end of the list
+  while(node !== null) {
+    // push each node value into the array
+    array.push(node.value);
+    // moving to the next node
+    node = node.rest;
+  }
+  return array;
 }
 
 function nth(list, position) {
-  // Your code here
+  // if the list is null, then there's no value so return undefined
+  if(list === null)
+    return undefined;
+  // else if the position reached is 0, then we retrieve the value from the node
+  else if(position === 0)
+    return list.value;
+  // else, we recur starting from the next node, and lowering the position
+  return nth(list.rest, --position);
 }
 
 function prepend(element, list) {
-  // Your code here
+  return {value: element, rest: list};
 }
 
 // Problem 4: Deep comparison
-function deepEqual(obj1, obj2) {
-  // Your code here
+function deepEqual(x, y) {
+  // if both inputs are objects and not null
+  if(typeof x === "object" && x !== null && typeof y === "object" && y !== null) {
+    // check if both objects have the same number of properties
+    if(Object.keys(x).length !== Object.keys(y).length)
+      return false;
+    // if they do, then loop through each property
+    for(var k in x) {
+      // make sure y has that property as well
+      if(!y.hasOwnProperty(k))
+        return false;
+      // do a recursion on the property's value
+      if(!deepEqual(x[k], y[k]))
+        return false;
+    }
+  }
+  // else if x and y are not objects but not equal, return false
+  else if(x !== y)
+    return false;
+  // else, the two inputs are equal so return true
+  return true;
 }
 
 
--- chapter13.js
+++ chapter13.js
@@ -9,7 +9,39 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+  var table = document.createElement("table");
+  var headerRow = document.createElement("tr");
+  var keys = Object.keys(data[0]);  // getting the keys of the first object
+
+  // looping through the keys and putting them in <th> tags
+  for(var i = 0; i < keys.length; i++) {
+    var th = document.createElement("th");
+    var key = document.createTextNode(keys[i]);
+    th.appendChild(key);
+    headerRow.appendChild(th);
+  }
+
+  // append the header row
+  table.appendChild(headerRow);
+
+  // loop through the objects and putting each object in its own row
+  for(var i = 0; i < data.length; i++) {
+    var tr = document.createElement("tr");
+    for(var k in data[i]) {
+      var td = document.createElement("td");
+      var element = data[i][k];
+
+      // if the element is a number, change the alignment and convert to string
+      if(typeof element === "number") {
+        td.style.textAlign = "right";
+        element = element.toString();
+      }
+      td.appendChild(document.createTextNode(element));
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  return table;
 }
 
 
