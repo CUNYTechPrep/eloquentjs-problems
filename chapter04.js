ff25e7df3b50660d1521d7cd92bb3e325d1e8626
QzSama <kelvinchao1996@gmail.com>
2017-08-28 09:43:34 +0800

    233

--- chapter04.js
+++ chapter04.js
@@ -9,43 +9,121 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  step=step||1;
+  var a=[];
+    
+    if(start<end&&step>0){
+      for(var i=start; i<=end;i=i+step)
+      {a.push(i);
+      }
+    }
+    
+    else if(end<start&&step<0){
+      for(var i=start; i>=end;i=i+step)
+      {a.push(i);
+      }}
+    
+    return a;  // Your code here
 }
 
 function sum(array) {
-  // Your code here
+  var s=0;
+    for(var i=0;i<=array.length-1;i++){
+        s=s+array[i];
+    }
+    return s;// Your code here
 }
 
 // Problem 2: Reversing an Array
 function reverseArray(array) {
-  // Your code here
+  var a1=[];
+  for(var i=array.length-1;i>=0;i--){
+    a1.push(array[i]);
+  }
+  return a1;  // Your code here
 }
 
 function reverseArrayInPlace(array) {
-  // Your code here
+  var temp=a[0];
+  var l=a.length
+  
+    for(var i=0;i<=l%2;i++){
+      temp=a[i];
+      a[i]=a[a.length-1-i];
+      a[a.length-1-i]=temp;
+    }  // Your code here
 }
 
 // Problem 3: A List
 function arrayToList(array) {
-  // Your code here
+  var s=array.shift();
+  if(s==undefined){return null;}
+  else{
+  var list={
+    value:s,
+    
+    rest:arrayToList(array)
+    
+  }
+  return list;}// Your code here
 }
 
 function listToArray(list) {
-  // Your code here
+  var a=[];
+  for(var node=list;node;node=node.rest)
+  { a.push(node.value);
+    }
+  return a;  // Your code here
 }
 
 function nth(list, position) {
-  // Your code here
+  while(pos){
+    list=list.rest;
+    position=position-1;
+  }
+  return list.value;  // Your code here
 }
 
 function prepend(element, list) {
-  // Your code here
+  var list={
+  value:element,
+  rest:list
+  }
+  return list;// Your code here
 }
 
 // Problem 4: Deep comparison
-function deepEqual(obj1, obj2) {
-  // Your code here
-}
+function deepEqual(a, b) {
+  if(a==b||a===b){return true}
+  else if(typeof a==typeof b){
+    if(typeof a=="object"){
+      if(a!=null && b!=null){
+        for ( var p in a ) {
+          if ( ! a.hasOwnProperty( p ) ) continue;
+
+          if ( ! b.hasOwnProperty( p ) ) return false;
+
+          if ( a[ p ] === b[ p ] ) continue;
+
+          if ( typeof( a[ p ] ) !== "object" ) return false;
+
+          if ( ! deepEqual( a[ p ],  b[ p ] ) ) return false;
+          }
+
+          for ( p in b ) {
+            if ( b.hasOwnProperty( p ) && ! a.hasOwnProperty( p ) ) return false;
+     
+          }
+          return true;
+      }         
+      
+      else return false;
+    }
+    else if (typeof a==number){return a==b}
+  }
+  else{return false;}}
+// Your code here
+
 
 
 // Do not modify below here.
--- chapter05.js
+++ chapter05.js
@@ -17,15 +17,26 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-
 // Problem 1: Flattening
 function flatten(arrays) {
+	var result=[];
+	for(var i=0;i<=arrays.length-1;i++){
+		result=result.concat(arrays[i]);
+	}
+	return result;
   // Your code here
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
+var hasKnownMother=function(person){
+	return person.mother in byName;
+}
+function momChildAgeDiff(){
+	return person.born- byName[person.mother].born;
+}
 function averageMomChildAgeDiff() {
+	return average(ancestry.filter(hasKnownMother).map(momChildAgeDiff));
   // Your code here
 }
 
@@ -33,7 +44,33 @@ function averageMomChildAgeDiff() {
 /* This must return the object/map with centuries as keys and average age
     for the century as the value
  */
+function personsCentury(person) {
+  return Math.ceil(person.died / 100);
+}
+function group(array){
+	var sorted={};//sorted={title(16-20):Array[n],tile2(16-20):Array[m]}
+	array.forEach(function(person){
+		var title=personsCentury(person);
+		if(title in sorted){
+			sorted[title].push(person);
+
+		}else{
+			sorted[title]=[person];
+		}
+	})
+	return sorted;
+}
+var sortedPeople=group(ancestry);
 function averageAgeByCentury() {
+	for(var century in sortedPeople){
+		var age=sortedPeople[title].map(function(person){
+			return person.died-person.born;
+
+
+		});
+		console.log(century+": "+average(age));
+		
+	}
   // Your code here
 }
 
