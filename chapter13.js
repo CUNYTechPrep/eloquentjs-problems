fc9b92fa66a98457308d794054c4061d1a8664a4
David-Bohl <davebohl@gmail.com>
2017-08-28 19:59:57 -0400

    solve chapter 4 5 and 13 problems

--- chapter13.js
+++ chapter13.js
@@ -9,7 +9,38 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+
+  // create table
+  var table = document.createElement('table');
+
+  // create header row
+  var headerRow = document.createElement('tr');
+
+  // populate header row with keys
+  let keys = Object.keys(data[0]);
+  keys.forEach( function(key){
+    var header = document.createElement('th');
+    header.innerHTML = key;
+    headerRow.appendChild(header);
+  })
+
+  // append header row
+  table.appendChild(headerRow);
+
+  // create row for each entry
+  for(var i = 0; i < data.length; i++){
+    var dataRow = document.createElement('tr');
+    // create data for each property
+    for(var j = 0; j < keys.length; j++){
+      var tableData = document.createElement('td');
+      tableData.innerHTML = data[i][keys[j]];
+      dataRow.appendChild(tableData);
+    }
+    table.appendChild(dataRow);
+  }
+
+  document.body.appendChild(table);
+  return table;
 }
 
 
