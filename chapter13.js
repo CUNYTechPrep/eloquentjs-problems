0eab2f1a06c57079171eb430e982fa6ef3177f5f
Brian C <brian_campbell@outlook.com>
2017-08-28 23:33:23 -0400

    aligned numbers to the right

--- chapter13.js
+++ chapter13.js
@@ -9,30 +9,33 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  //Build headings
-  var myTable = document.createElement("table");
-  var headings = document.createElement("tr");
-  for(var key in data[0]){
-    var node = document.createElement("th");
-    var textNode = document.createTextNode(key);
-    node.appendChild(textNode);
-    headings.appendChild(node);
-  }
-  myTable.appendChild(headings);
-  //Build the entries
-  for(var x=0; x<data.length; ++x){
-    var entry = data[x];
-    var row = document.createElement("tr");
-    for(var key in entry){
-      var item = document.createElement("td");
-      var itemText = document.createTextNode(entry[key]);
-      item.append(itemText);
-      row.append(item);
+    //Build headings
+    var myTable = document.createElement("table");
+    var headings = document.createElement("tr");
+    for(var key in data[0]){
+      var node = document.createElement("th");
+      var textNode = document.createTextNode(key);
+      node.appendChild(textNode);
+      headings.appendChild(node);
     }
-    myTable.append(row);
+    myTable.appendChild(headings);
+    //Build the entries
+    for(var x=0; x<data.length; ++x){
+      var entry = data[x];
+      var row = document.createElement("tr");
+      for(var key in entry){
+        var item = document.createElement("td");
+        var itemText = document.createTextNode(entry[key]);
+        item.append(itemText);
+        if(!isNaN(entry[key])){
+          item.style.textAlign = "right";
+        }
+        row.append(item);
+      }
+      myTable.append(row);
+    }
+    return myTable;
   }
-  return myTable;
-}
 
 
 // Do not modify below here.
