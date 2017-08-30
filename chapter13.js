b5c2105082a0b2c17e7a168ef5495404c239940a
Simon Mai <maismin@gmail.com>
2017-08-30 15:39:29 -0400

    Complete buildTable function

--- chapter13.js
+++ chapter13.js
@@ -9,7 +9,39 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+    var table = document.createElement("table");
+    var row,
+        properties,
+        i, j,
+        headerEle,
+        dataEle,
+        dataObj,
+        text;
+    if (data.length > 0) {
+      //Create table header rows and append to table variable
+      row = table.appendChild(document.createElement("tr"));
+      properties = Object.keys(data[0]);
+
+      for (i = 0; i < properties.length; i++) {
+        headerEle = document.createElement("th");
+        text = document.createTextNode(properties[i]);
+        headerEle.appendChild(text);
+        row.appendChild(headerEle);
+      }
+
+      // create rest of the table
+      for (i = 0; i < data.length; i++) {
+        row = table.appendChild(document.createElement("tr"));
+        dataObj = data[i];
+        for (j = 0; j < properties.length; j++) {
+          dataEle = document.createElement("td");
+          text = document.createTextNode(dataObj[properties[j]]);
+          dataEle.appendChild(text);
+          row.appendChild(dataEle);
+        }
+      }
+    }
+    return table;
 }
 
 
