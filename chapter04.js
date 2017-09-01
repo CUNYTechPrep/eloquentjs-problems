3162e360565a358049412b824b631a691fffe42f
Janiza G <hi@janizagesmundo.com>
2017-09-01 10:39:22 -0400

    Update chapter04.js

--- chapter04.js
+++ chapter04.js
@@ -36,16 +36,8 @@ function sum(array) {
   }
   return total;
 };
-
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
-
-
 }
+
 
 
 
@@ -68,17 +60,10 @@ function reverseArrayInPlace(array) {
   return array;
 }
 
-console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
-var arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
-// → [5, 4, 3, 2, 1]
 
 
 
 // Problem 3: A List
-
 function arrayToList (array) {
   var list = null;
   for (var i = array.length - 1; i >= 0; i--) {
@@ -109,22 +94,10 @@ function prepend (element, list) {
   return list;
 }
 
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
-
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
 
 
 
 // Problem 4: Deep comparison
-
 var deepEqual = function (obj1, obj2) {
   if (obj1 === obj2) { 
     return true;
@@ -149,14 +122,7 @@ var deepEqual = function (obj1, obj2) {
  
   return propertyInX === propertyInY;
 };
- 
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// true
+
 
 
 
