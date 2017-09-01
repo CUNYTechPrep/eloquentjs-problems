600fa91e54f575e2d3372a3d9a2627513fb4f15f
Enzo <e.amespizarro@gmail.com>
2017-09-01 18:11:03 -0400

    passed all tests

--- chapter05.js
+++ chapter05.js
@@ -17,16 +17,24 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-
 // Problem 1: Flattening
 function flatten(arrays) {
-  // Your code here
+  return arrays.reduce( (a, b) => {return a.concat(b); }, []); 
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
+
 function averageMomChildAgeDiff() {
-  // Your code here
+  let ageDifferences = [];
+  let filteredAncestry = ancestry.filter( (person) => {return byName[person.mother] != null ? true : false;} );
+  filteredAncestry.forEach( (person) => {
+    let birth = person.born;
+    let motherDOB = byName[person.mother].born;
+    let ageDiff = birth - motherDOB;
+    ageDifferences.push(ageDiff); 
+  });
+  return average(ageDifferences);
 }
 
 // Problem 3: Historical life expectancy
@@ -34,8 +42,22 @@ function averageMomChildAgeDiff() {
     for the century as the value
  */
 function averageAgeByCentury() {
-  // Your code here
+  averageAges = {};
+  ancestry.forEach((person) => {
+    let century = Math.ceil(person.died / 100);
+    let age = person.died - person.born;
+    if (!averageAges.hasOwnProperty(century)) {
+      averageAges[century] = [age];
+    } else {
+      averageAges[century].push(age);
+    }
+  });
+  Object.keys(averageAges).forEach( (century) => { averageAges[century] = average(averageAges[century]); } )
+  return averageAges;
 }
+
+
+
 
 
 // Do not modify below here.
--- chapter13.js
+++ chapter13.js
@@ -9,9 +9,45 @@
 
 // Problem 1: Build table
 function buildTable(data) {
-  // Your code here
+  let table = document.createElement("table");
+  let headings = Object.keys(data[0]);
+  let headerRow = createRow(headings, true);
+  table.appendChild(headerRow);
+  data.forEach( (element) => {
+    let rowData = Object.keys(data[0]).map((key) => {
+      return element[key];
+    });
+    let row = createRow(rowData);
+    table.appendChild(row);
+  });
+  return table
 }
 
+function createRow(data, isHeader) {
+  let row = document.createElement("tr");
+  data.forEach((element) => {
+    let col = createCol(element, isHeader);
+    row.appendChild(col);
+  });
+  return row;
+};
+
+function createCol(data, isHeader) {
+  let col;
+  if (isHeader){
+    col = document.createElement("th");
+  }
+  else{ 
+    col = document.createElement("td");
+  }
+
+  let content = document.createTextNode(data);
+  col.appendChild(content);
+  if (typeof data == "number"){
+    col.style.textAlign = "right";
+  }
+  return col;
+}
 
 // Do not modify below here.
 module.exports = { buildTable };
