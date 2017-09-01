cc29eb93da91dc09bdd88d62074091583a126e32
Esther Song <esther.song62@myhunter.cuny.edu>
2017-08-31 23:54:36 -0400

    add solution for build table

--- chapter13.js
+++ chapter13.js
@@ -10,6 +10,35 @@
 // Problem 1: Build table
 function buildTable(data) {
   // Your code here
+  var table = document.createElement("table");
+  var table_row = document.createElement("tr");
+  var thead_data = Object.keys(data[0]);
+
+  for(var i = 0; i < thead_data.length; i++){
+  	var head_element = thead_data[i];
+  	var table_head = document.createElement("th");
+  	table_head.appendChild(document.createTextNode(head_element));
+  	table_row.appendChild(table_head);
+  }
+  table.appendChild(table_row);	//table header
+
+  for(var k = 0; k < data.length; k++){
+  	table_row = document.createElement("tr");
+  	//add all three
+  	for(var m = 0; m < thead_data.length; m++){
+  		var head_col_name = thead_data[m];
+  		var table_data = document.createElement("td");
+  		var current_obj = data[k];
+  		table_data.appendChild(document.createTextNode(current_obj[head_col_name]));
+
+  		if(typeof current_obj[head_col_name] == "number"){
+  			table_data.style.textAlign = "right";
+  		}
+  		table_row.appendChild(table_data);
+  	}
+  	table.appendChild(table_row);
+  }
+  return table;
 }
 
 
