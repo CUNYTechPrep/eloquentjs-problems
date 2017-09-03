e915bdd5a75497514efcb97a17d002f3c6d9c516
xiaohang zou <xzou000@citymail.cuny.edu>
2017-09-03 14:30:25 -0400

    first

--- chapter05.js
+++ chapter05.js
@@ -21,12 +21,33 @@ ancestry.forEach(function(person) {
 // Problem 1: Flattening
 function flatten(arrays) {
   // Your code here
+  console.log(arrays.reduce(function(a, b){return a.concat(b)},[]));
+  
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
   // Your code here
+	function average(array) {
+		function plus(a, b) { return a + b; }
+		return array.reduce(plus) / array.length;
+	}
+
+	var byName = {};
+	ancestry.forEach(function(person) {
+		byName[person.name] = person;
+	});
+
+	// Your code here.
+	var hasKnownMother = function(person) {
+		return person.mother in byName;
+	};
+
+	function getAgeDiff(person) {
+		return person.born - byName[person.mother].born
+	}
+	console.log(Number(average(ancestry.filter(hasKnownMother).map(getAgeDiff)).toFixed(1)));
 }
 
 // Problem 3: Historical life expectancy
@@ -35,6 +56,17 @@ function averageMomChildAgeDiff() {
  */
 function averageAgeByCentury() {
   // Your code here
+  var defined_century ={};
+	ancestry.forEach(function(person) {
+		var century = Math.ceil(person.died / 100);
+		if(!(century in defined_century))
+			defined_century[century] = [];
+		defined_century[century].push(person.died - person.born);
+	});	
+	for(var item in defined_century){
+	  defined_century[item] = Number(average(defined_century[item]).toFixed(1));
+	}
+	console.log(defined_century);
 }
 
 
